41c83a01879d22794245751c5481941eb86fef8a
GOMEZ Gerardo <aez@tgn.com.ar>
2021-04-15 01:43:15 -0300

    sarlanga

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
 
 const prueba = fs.readFileSync(__dirname + '//\//tareas.json','utf-8')
+//const prueba = fs.readFileSync(__dirname + '//\//Prueba.json','utf-8')
 console.log(prueba)
+
+const contenidoJson = JSON.parse(prueba)
+
+console.log(contenidoJson)
